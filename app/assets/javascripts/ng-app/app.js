eb8b79d8c87e39e3536e1a85af99a7eb16d03eba
jquigley1003 <jeff.quigley@wavinghi.com>
2014-12-16 18:22:05 -0500

    changes to .gitignore

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -12,7 +12,30 @@ angular.module('YeaNayers', [
             url: '/',
             templateUrl: 'home.html',
             controller: 'HomeCtrl'
-        });
+        })
+
+        // an abstract state that just serves as a
+        // parent for the below child states
+        .state('dashboard', {
+            abstract: true,
+            url: '/dashboard',
+            templateUrl: 'dashboard/layout.html'
+        })
+            // the default route when someone hits dashboard
+            .state('dashboard.one', {
+                url: '',
+                templateUrl: 'dashboard/one.html'
+            })
+            // this is /dashboard/two
+            .state('dashboard.two', {
+                url: '/two',
+                templateUrl: 'dashboard/two.html'
+            })
+            // this is /dashboard/three
+            .state('dashboard.three', {
+                url: '/three',
+                templateUrl: 'dashboard/three.html'
+            });
 
     // default fall back route
     // $urlRouterProvider.otherwise('/');
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,14 @@ module Yeanayers
   # end
 
   class Application < Rails::Application
+
+    config.assets.paths << 
+    Rails.root.join("lib","assets","bower_components",
+        "bootstrap-sass-official", "assets", "stylesheets")
+
+    config.assets.paths << 
+    Rails.root.join("lib","assets","bower_components",
+        "bootstrap-sass-official", "assets","fonts")    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
