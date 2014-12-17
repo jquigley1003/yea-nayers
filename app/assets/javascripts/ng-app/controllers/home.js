e78c16d42aa07e51a2c67dd30769aff5eb780afa
jquigley1003 <jeff.quigley@wavinghi.com>
2014-12-17 17:42:48 -0500

    map update and sample api call

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,4 +1,12 @@
 angular.module('YeaNayers')
-  .controller('HomeCtrl', ['$scope', function($scope) {
+  .controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.things = ['Angular', 'Rails 4.1', 'UI Router', 'Together!!'];
+    
+
+ 
+      var govTrackUrl = 'https://www.govtrack.us/api/v2/bill?q=gun%20control&congress=113&order_by=-current_status_date';
+      $http.get(govTrackUrl).success(function(data){
+      $scope.bills = data;
+      });
+
   }]);
